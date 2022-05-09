2db0f5fdebc0b24e15346ad6d8e569c57b214070
Brian Franze <brian.franze@denbury.com>
2022-05-09 08:54:48 -0500

    checkin

--- ExpensesAPI/Controllers/EntriesController.cs
+++ ExpensesAPI/Controllers/EntriesController.cs
@@ -10,7 +10,7 @@ using System.Web.Http.Cors;
 
 namespace ExpensesAPI.Controllers
 {
-    [EnableCors("*","*","*")]
+    [EnableCors("*", "*","*")]
     public class EntriesController : ApiController
     {
         [AcceptVerbs("GET")]
